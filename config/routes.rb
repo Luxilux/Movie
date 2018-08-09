c97472833e84841971419ab3ce6e63838d851ffd
steven@forban.io <steven@MacBook-Air-de-viardot.local>
2018-08-09 22:10:46 +0200

    last

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/search', to: 'movies#search'
+  post '/search', to: 'movies#search'
   get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
